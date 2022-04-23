fb0db6476e833baa41fa1ca068f721f65c84e5b9
CJRockwell5 <104281377+CJRockwell5@users.noreply.github.com>
2022-04-23 19:25:19 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ bot_chance = st.slider('Chance of Botryis', min_value=0, max_value=100)
 sug_high = st.slider('Chance of High Sugar', min_value=0, max_value=100)
 sug_med = st.slider('Chance of Medium Sugar', min_value=0, max_value=100)
 sug_low = st.slider('Chance of Low Sugar', min_value=0, max_value=100)
-st.text(bot_chance)
 
-
-st.button("Update")
+if bot_chance*95.2*275000 > (sug_med + sug_high)*95.2*(125000 + 117500):
+  st.text(bot_chance)
+else:
+  st.text(sug_med + sug_high)
