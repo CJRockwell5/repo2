e36be3b295ce101c6490c9d0535e11b2a201d946
CJRockwell5 <104281377+CJRockwell5@users.noreply.github.com>
2022-04-23 19:14:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit as st
-st.slider('Slide me', min_value=0, max_value=100)
-st.slider('Slide me', min_value=0, max_value=10)
+bot_chance = st.slider('Chance of Botryis', min_value=0, max_value=100)
+sug_high = st.slider('Chance of High Sugar', min_value=0, max_value=100)
+sug_med = st.slider('Chance of Medium Sugar', min_value=0, max_value=100)
+sug_low = st.slider('Chance of Low Sugar', min_value=0, max_value=100)
+st.text(bot_chance)
+
+
+st.button("Update")
