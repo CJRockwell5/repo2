1e23f97fcd520aa9810555dc6e7c6ff0aead5112
CJRockwell5 <104281377+CJRockwell5@users.noreply.github.com>
2022-04-23 19:39:26 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,11 +9,11 @@ if (sug_high + sug_med + sug_low) == 100:
     st.text('Best outcome: Botrytis Mold, chance:')
     st.text(bot_chance)
     if bot_chance*95.2*275000 < (100 - bot_chance)*95.2*35000:
-    st.text('Benifits do not outweight downside, harvest now')
+      st.text('Benifits do not outweight downside, harvest now')
   else:
     st.text('Best outcome: Medium and High Sugar, chance:')
     st.text(sug_med + sug_high)
     if (sug_med + sug_high)*95.2*(125000 + 117500) < sug_low*95.2*80000:
-    st.text('Benifits do not outweight downside, harvest now')
+      st.text('Benifits do not outweight downside, harvest now')
 else:
   st.text('High, Medium, and Low have to add up to 100')
