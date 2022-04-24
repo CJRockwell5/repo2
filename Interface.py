2fdc96e8a5b67c3c8ce218ac6b774906947c150f
CJRockwell5 <104281377+CJRockwell5@users.noreply.github.com>
2022-04-24 13:01:17 -0400

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -2,6 +2,14 @@ import streamlit as st
 st.header('Pneumonia Classification')
 st.subheader('Group 5')
 
-uploaded_file = st.file_uploader("Choose a X-ray")
+image_file = st.file_uploader("Upload a X-ray", type=["png","jpg","jpeg"])
 if uploaded_file is not None:
      input_image = uploaded_file.getvalue()
+     
+     # To See details
+     file_details = {"filename":image_file.name, "filetype":image_file.type,
+     "filesize":image_file.size}
+     st.write(file_details)
+
+     # To View Uploaded Image
+     st.image(load_image(image_file),width=250)
