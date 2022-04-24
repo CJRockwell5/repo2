9e4ae56364de6e894239d36ba8fda40e1b78892e
CJRockwell5 <104281377+CJRockwell5@users.noreply.github.com>
2022-04-24 12:49:13 -0400

    Add files via upload

---
+++ Interface.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.header('Pneumonia Classification')
+st.subheader('Group 5')
+st.file_uploader(x-ray, type=['png', 'jpg'], accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, *, disabled=False)
